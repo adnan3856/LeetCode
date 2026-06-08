0ddffad0c9c91beefd5fc32020fbeaca56f7016a
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-08 18:18:18 +0530

    Time: 1 ms (88.80%) | Memory: 44.3 MB (19.78%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,21 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        // Start with the empty set
-        result.add(new ArrayList<>());
-
-        for (int num : nums) {
-            int size = result.size();
-            // Iterate through existing subsets and expand them
-            for (int i = 0; i < size; i++) {
-                List<Integer> newSubset = new ArrayList<>(result.get(i));
-                newSubset.add(num);
-                result.add(newSubset);
-            }
-        }
+        backtrack(nums, 0, new ArrayList<>(), result);
         return result;
+    }
+
+    public void backtrack(int[] nums, int index, List<Integer> current, List<List<Integer>> result ) {
+        result.add(new ArrayList<>(current));
+
+        for(int i=index; i< nums.length; i++) {
+            current.add(nums[i]);
+            backtrack(nums, i+1, current, result);
+
+            current.remove(current.size() - 1);
+        }
     }
 }
