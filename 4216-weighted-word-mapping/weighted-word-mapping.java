c91b2898f11f6550e17fb63716ba2cc1fd201230
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-13 18:31:57 +0530

    Time: 2 ms (96.85%) | Memory: 46.4 MB (81.55%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.java
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -1,13 +1,13 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
-        String res = "";
+        StringBuilder res = new StringBuilder();
         for(String word: words) {
             int sum = 0;
             for(char ch : word.toCharArray()) {
                 sum += weights[(int)ch - 'a'];
             }
-            res += (char)((int)'z' - sum % 26) ;
+            res.append((char)((int)'z' - sum % 26)) ;
         }
-        return res;
+        return res.toString();
     }
 }
