324d5e830a2a891acc1a8b9997a302d6c8d60145
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-16 12:04:41 +0530

    Time: 44 ms (3.74%) | Memory: 46.5 MB (5.78%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for( int i = 2; i<=n; i++) {
+            System.out.println("i : " + i);
+            char current = s.charAt(i-1);
+            System.out.println("current : " + current);
+
+            if(current != '0')
+                dp[i] += dp[i-1];
+            
+            System.out.println("dp[i] : " + dp[i] + ", dp[i-1]: " + dp[i-1]);
+
+            int twoDigit = (s.charAt(i-2) - '0') * 10 + (s.charAt(i-1) - '0');
+            System.out.println("twoDigits : " + twoDigit);
+
+            if(twoDigit >= 10 && twoDigit <= 26)
+                dp[i] += dp[i-2];
+            
+            System.out.println("dp[i] : " + dp[i] + ", dp[i-2]: " + dp[i-2]);
+        }
+        return dp[n];
+    }
+}
