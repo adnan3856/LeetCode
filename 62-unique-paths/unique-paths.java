62626b4fba52fa6dbcc87f3736f700b9165a6659
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-16 17:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (64.97%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,18 +1,28 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        // Define state: dp[i][j] = number of ways to reach cell (i,j)
         int[][] dp = new int[m][n];
 
-        for(int i=0; i< m; i++)
+        // Base case
+        // First row
+        for(int i=0; i< n; i++)
+            dp[0][i] = 1;
+        
+        // First column
+        for(int i=0; i<m; i++)
             dp[i][0] = 1;
 
-        for(int i=0; i<n; i++)
-            dp[0][i] = 1;
-
-        for(int i=1; i<m; i++) {
-            for(int j=1; j<n; j++) {
+        // Define Choices: To reach (i,j), the robot can come from:
+        // Top    -> (i-1,j)
+        // Left   -> (i,j-1)
+        for(int i=1; i<m; i++) { // row
+            for(int j=1; j<n; j++) { // column
+                // Top + left
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         return dp[m-1][n-1];
     }
 }
+
+
