bdfb3f34135034d5052a4b01d567b274a9a61ec5
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-17 19:11:22 +0530

    Time: 17 ms (47.21%) | Memory: 47 MB (28.39%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,20 +1,25 @@
-import java.util.Arrays;
-
 class Solution {
     public int coinChange(int[] coins, int amount) {
+         
+         int[][] dp = new int[coins.length + 1][amount+1];
 
-        int[] dp = new int[amount + 1];
-
-        Arrays.fill(dp, amount + 1); // 12
-        dp[0] = 0;
-
-        for(int i=1; i <= amount; i++) {
-            for(int coin : coins) {
-                if(coin <= i) 
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-            }
+         // Initialize impossible states
+        for (int j = 1; j <= amount; j++) {
+            dp[0][j] = amount + 1;
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+
+        for(int i=1; i<=coins.length; i++) {
+            int coin = coins[i-1];
+            for(int j=0; j<= amount; j++) {
+
+                dp[i][j] = dp[i-1][j];
+
+                if(j >= coin) {
+                    dp[i][j] = Math.min (dp[i-1][j], 1 + dp[i][j - coin]);
+                }
+            }
+        }
+        return dp[coins.length][amount] > amount ? -1 : dp[coins.length][amount];
     }
 }
