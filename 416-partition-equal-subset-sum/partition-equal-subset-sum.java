ceaf918b4155aa124edac9367bb11bab108b6566
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-17 18:22:52 +0530

    Time: 66 ms (36.13%) | Memory: 47.4 MB (61.09%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,21 +1,38 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        // first we are trying to figure out the sum as the target can be decided
         int sum = 0;
-        for(int num : nums)
+
+        for(int num : nums) 
             sum += num;
-        if(sum%2 != 0)
-            return false;
         
+        if(sum%2 != 0) // if the sum is not even ,the partition cannot be done 
+        // ((sum & 1) == 1) bitwise way of even odd, 
+            return false;
+
         int target = sum / 2;
-        boolean[] dp = new boolean[target+1];
-        dp[0] = true;
-        for(int num : nums) {
-            for(int i=target; i >= num; i--) {
-                if(dp[i - num])
-                    dp[i] = true;
+
+        boolean[][] dp = new boolean[nums.length+1][target+1];
+
+        // fill the column and row with true, as base case
+        for(int i=0; i<=nums.length; i++)
+            dp[i][0] = true;
+        
+        // for(int j=0; j<= target; j++)
+        //     dp[0][j] = true;
+
+        for(int i=1; i <= nums.length; i++) {
+            int current = nums[i - 1];
+            for(int j=1; j <= target; j++) {
+                // not take / skip
+                dp[i][j] = dp[i-1][j];
+
+                // take
+                if(j >= current) {
+                    dp[i][j] = dp[i][j] || dp[i-1][j-current];
+                }
             }
         }
-        return dp[target]; 
+        return dp[nums.length][target];
     }
-
 }
