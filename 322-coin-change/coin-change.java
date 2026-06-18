cabbfab201f26d43e047439b4a03a8051de840bb
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-18 18:00:55 +0530

    Time: 17 ms (47.28%) | Memory: 47.1 MB (19.52%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-         
-         int[][] dp = new int[coins.length + 1][amount+1];
+        // Question to ask: will the minimum of i coins make amount j
+        int[][] dp = new int[coins.length+1][amount+1];
 
-         // Initialize impossible states
-        for (int j = 1; j <= amount; j++) {
-            dp[0][j] = amount + 1;
+        // expecting that any number of coins cannot make amonut, except 0 coins
+        for(int i=1; i<=amount; i++) {
+            dp[0][i] = amount+1;
         }
 
-
-        for(int i=1; i<=coins.length; i++) {
+        for(int i = 1; i<= coins.length; i++) {
             int coin = coins[i-1];
-            for(int j=0; j<= amount; j++) {
 
+            for(int j=0; j<= amount; j++) {
+                
+                // not take
                 dp[i][j] = dp[i-1][j];
 
                 if(j >= coin) {
-                    dp[i][j] = Math.min (dp[i-1][j], 1 + dp[i][j - coin]);
+                    dp[i][j] = Math.min(dp[i-1][j], 1+ dp[i][j-coin]);
                 }
             }
         }
