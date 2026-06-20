b3723571eb75d95cd3dbfe5b2f4e91b1a748d07e
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-20 15:49:42 +0530

    Time: 67 ms (35.42%) | Memory: 47.3 MB (61.16%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -2,29 +2,26 @@ class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
 
-        for(int i=0; i< nums.length; i++) 
-            sum += nums[i];
-
-        if(sum%2 != 0)
+        for(int num : nums)
+            sum += num;
+        
+        if(sum %2 != 0)
             return false;
 
         int target = sum / 2;
 
-        // can i elements make sum j
-        boolean[][] dp = new boolean[nums.length+1][target + 1];
+        boolean[][] dp = new boolean[nums.length+1][target+1];
 
-        for(int i=0; i< nums.length; i++)
-            dp[i][0] = true;
+        for(int i = 0; i <= nums.length; i++) 
+            dp[i][0] =true;
 
-        for(int i=1; i <= nums.length; i++) {
+        for(int i=1; i<=nums.length; i++) {
             int current = nums[i-1];
-            for(int j =0; j <= target; j++) {
-                // skip
+            for(int j=1; j<=target; j++) {
                 dp[i][j] = dp[i-1][j];
 
-                if(j >= current) {
-                    dp[i][j] = dp[i][j] || dp[i-1][j - current];
-                }
+                if(j >= current)
+                    dp[i][j] = dp[i][j] || dp[i-1][j-current];
             }
         }
         return dp[nums.length][target];
