11f5106d37fe9dbbd2e464cc120280dfb909dfef
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-23 18:20:03 +0530

    Time: 2 ms (90.75%) | Memory: 44.3 MB (40.01%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,33 +1,28 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-
-        // queue stores where are rotten oranges
         Queue<int[]> queue = new LinkedList<>();
 
-        int fresh = 0; // to check how many fresh oranges are present
+        int fresh = 0;
 
-        for(int i=0; i< grid.length; i++) {
+        for(int i=0; i<grid.length; i++) {
             for(int j=0; j<grid[0].length; j++) {
-                if(grid[i][j] == 2) {
+                if(grid[i][j] == 2)
                     queue.offer(new int[]{i, j});
-                }
                 if(grid[i][j] == 1)
                     fresh++;
             }
         }
 
         if(fresh == 0) return 0;
-        int minutes = 0;
 
-        int[][] directions = {{+1, 0} , {-1, 0}, {0, +1}, {0, -1}};
+        int minutes=0;
+        int[][] directions = {{+1, 0}, {-1, 0}, {0, +1}, {0, -1}};
 
         while(!queue.isEmpty() && fresh > 0) {
             int size = queue.size();
-
             boolean rotten = false;
-
-            for(int i=0; i<size; i++) {
-                int[] current = queue.poll(); // find the 1, 2, 3... rotten postion
+            for(int i = 0; i< size; i++) {
+                int[] current = queue.poll();
 
                 int row = current[0];
                 int col = current[1];
@@ -36,20 +31,17 @@ class Solution {
                     int nr = row + direction[0];
                     int nc = col + direction[1];
 
-                    // check for valid, all directions should be vaild and orange should be fresh
-                    if(nr >= 0 && nc >= 0 &&
-                        nr < grid.length && nc < grid[0].length &&
-                            grid[nr][nc] == 1) {
-                                grid[nr][nc] = 2;
-                                fresh--;
-                                queue.offer(new int[]{nr, nc});
-                                rotten = true;
-                            }
+                    if(nr >= 0 && nc >= 0 && nr < grid.length && nc <grid[0].length && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        fresh--;
+                        queue.offer(new int[]{nr, nc});
+                        rotten = true;
+                    }
                 }
             }
             if(rotten)
                 minutes++;
         }
-        return fresh == 0 ? minutes: -1;
+        return fresh == 0 ? minutes : -1;
     }
 }
