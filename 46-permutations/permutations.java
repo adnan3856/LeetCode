44db3c53eb824db96b89d0b30abe1f6de734c05c
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-23 18:27:41 +0530

    Time: 1 ms (87.82%) | Memory: 45.4 MB (63.38%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -8,18 +8,19 @@ class Solution {
         return result;
     }
 
-    public void backtrack(int[] nums, boolean[] visited, List<Integer> current, List<List<Integer>> result ) {
+    public void backtrack(int[] nums, boolean[] visited, List<Integer> current, List<List<Integer>> result) {
+
         // base
-        if(current.size() == nums.length){
+        if(current.size() == nums.length) {
             result.add(new ArrayList<>(current));
             return;
         }
 
-        for (int i=0; i < nums.length; i++) {
+        for(int i=0; i<nums.length; i++) {
             
             if(visited[i])
                 continue;
-            //choose
+            // choose
             visited[i] = true;
             current.add(nums[i]);
 
