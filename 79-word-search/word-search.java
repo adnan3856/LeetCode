f064d4cf3de6741b0b78972b38bc55f0805ff74e
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-06-25 18:17:33 +0530

    Time: 133 ms (61.47%) | Memory: 42.9 MB (75.13%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int rows = board.length;
-        int cols = board[0].length;
-
-        for(int row=0; row < rows; row++) {
-            for(int col=0; col < cols; col++) {
+        
+        for(int row = 0; row < board.length; row++) {
+            for(int col=0; col < board[0].length; col++) {
                 if(dfs(board, word, 0, row, col))
                     return true;
             }
@@ -16,28 +14,20 @@ class Solution {
         if(index == word.length())
             return true;
 
-        // edge cases
-        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length )
+        if(row < 0 ||col < 0 || row>=board.length || col >=board[0].length)
             return false;
         
-        // Character mismatch from board to word
         if(board[row][col] != word.charAt(index))
             return false;
 
-        // choose
         char temp = board[row][col];
         board[row][col] = '#';
 
-        // explore
-        boolean found = dfs(board, word, index + 1, row+1, col) ||
-                        dfs(board, word, index + 1, row-1, col) ||
-                        dfs(board, word, index + 1, row, col + 1) ||
-                        dfs(board, word, index + 1, row, col - 1);
-
-        // unchoose
-        // Restore original character
+        boolean found = dfs(board, word, index+1, row+1, col) ||
+                        dfs(board, word, index+1, row-1, col) ||
+                        dfs(board, word, index+1, row, col+1) ||
+                         dfs(board, word, index+1, row, col-1);
         board[row][col] = temp;
         return found;
-
     }
 }
