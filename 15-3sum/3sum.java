d58c8fe37c4082184ee80ef19703c35014ba2360
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-07-07 10:48:50 +0530

    Time: 45 ms (18.70%) | Memory: 59.6 MB (21.05%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,6 +3,9 @@ class Solution {
         Set<List<Integer>> result = new HashSet<>();
         Arrays.sort(nums);
         for(int i =0; i< nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+            
             int left = i+1, right = nums.length - 1;
             while(left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
