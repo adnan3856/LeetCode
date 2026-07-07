49a93acb02b6789f73c35a510023e7b76d592cd4
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-07-07 12:29:15 +0530

    Time: 3 ms (87.65%) | Memory: 52.4 MB (46.23%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,32 +1,36 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int islands = 0;
 
-        for(int i=0; i< grid.length; i++) {
-            for(int j=0; j<grid[0].length; j++) {
-                if(grid[i][j] == '1') {
-                    islands++;
-                    dfs(grid, i, j);
+    private final int[][] DIRS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int island = 0;
+        for(int row=0; row<rows; row++) {
+            for(int col=0; col < cols; col++) {
+                if(grid[row][col] == '1') {
+                    island++;
+                    dfs(grid, row, col);
                 }
             }
         }
-        return islands;
+        return island;
     }
 
     public void dfs(char[][] grid, int row, int col) {
         // validate
-            if(row < 0 || col < 0 ||
-                row >= grid.length || col >= grid[0].length ||
-                grid[row][col] == '0') {
-                    return;
-                }
-        // visited
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0')
+            return;
+        
+        //visit
         grid[row][col] = '0';
 
         // explore
-        dfs(grid, row+1, col);
-        dfs(grid, row-1, col);
-        dfs(grid, row, col+1);
-        dfs(grid, row, col-1);
+        for(int[] dir : DIRS)
+            dfs(grid, row+dir[0], col+dir[1]);
+        // dfs(grid, row+1, col);
+        // dfs(grid, row-1, col);
+        // dfs(grid, row, col+1);
+        // dfs(grid, row, col-1);
     }
 }
