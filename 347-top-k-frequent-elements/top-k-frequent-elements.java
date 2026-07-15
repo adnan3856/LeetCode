4ba85748052ae1b6dc763277e3982f1547c6cb2b
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-07-15 18:37:08 +0530

    Time: 16 ms (39.25%) | Memory: 47.9 MB (12.92%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        return Arrays.stream(nums)
-                .boxed()
-                // Step 1: Group and count frequencies
-                .collect(Collectors.groupingBy(
-                        Function.identity(), 
-                        Collectors.counting()
-                ))
-                // Step 2: Stream the map entries
-                .entrySet()
-                .stream()
-                // Step 3: Sort by frequency in descending order
-                .sorted(Map.Entry.<Integer, Long>comparingByValue().reversed())
-                // Step 4: Keep only the top K entries
-                .limit(k)
-                // Step 5: Unbox the key (Integer) back to a primitive int
-                .mapToInt(Map.Entry::getKey)
-                // Step 6: Collect elements into a primitive int[] array
-                .toArray();
-                     
+        int[] topKFrequent = new int[k];
+      Map<Integer,Integer> countMap = new HashMap<>();
+      for(int num : nums){
+        countMap.put(num,countMap.getOrDefault(num,0)+1 );
+      }
+      PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> countMap.get(a) - countMap.get(b));
+    
+    for(int frequency : countMap.keySet()){
+        minHeap.offer(frequency);
+        if(minHeap.size()>k){
+            minHeap.poll();
+        }
+    }
+    for(int i=k-1; i>=0; i--){
+       topKFrequent[i] =  minHeap.poll();
+    }
+    return topKFrequent;
+
     }
 }
